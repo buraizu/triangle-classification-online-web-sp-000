bf3838818becec921e35ca1600ba8fb5ad409a97
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-14 06:53:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,11 @@ class Triangle
   end
 
   def kind
-    if @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2 
+    if @side1 <= 0 || @side2 <=0 || @side3 <= 0
       begin
         raise TriangleError
       rescue TriangleError => error
-        puts error.message
+        return error.message
       end
     end
     if @side1 == @side2 && @side2 == @side3
