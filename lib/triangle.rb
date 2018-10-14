ea2cbe0578c64a7dd7c5106bef598f0c5bf97f9b
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-14 06:46:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       rescue TriangleError => error
         puts error.message
       end
-    elsif @side1 == @side2 && @side2 == @side3
+    if @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
       :scalene
