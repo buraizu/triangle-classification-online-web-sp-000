434ffc3d2796b64a6c7cdd970538bda9c2157c57
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-14 06:14:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,11 +10,24 @@ class Triangle
 
   def kind
     if @side1 + @side2 !> @side3 || @side2 + @side3 !> @side1 || @side1 + @side3 !> @side2
-      
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      :scalene
+    else
+      :isosceles
+    end
   end
 
   class TriangleError < StandardError
-    
+    def message
+      "The lengths entered are invalid to create a triangle."
+    end
   end
 
 end
