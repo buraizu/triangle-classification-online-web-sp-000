7aa1ab0f9fd8bf55b253dcc7158a0bbdc54c9d32
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-14 07:27:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,10 +14,11 @@ class Triangle
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
       begin
         raise TriangleError
-        rescue TriangleError => error
+
         puts error.message
-      
+
         is_valid = false
+      end
       end
 
     if is_valid == true && @side1 == @side2 && @side2 == @side3
